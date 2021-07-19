d74bcbd598003ef4a7dd9e75ff45926a47e59834
Seak Digital <nsineke@gmail.com>
2021-07-19 14:44:09 +0200

    chore(App): Init next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import Image from "next/image";
+import { PaystackButton } from "react-paystack";
+import styles from "../styles/Home.module.css";
+
+const publicKey = process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY;
 
 export default function Home() {
+  const amount = 100; // Remember, set in kobo!
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      name,
+      phone,
+    },
+    publicKey,
+    currency: "zar",
+    text: "Pay Now",
+    onSuccess: () =>
+      alert("Thanks for doing business with us! Come back soon!!"),
+    onClose: () => alert("Wait! Don't leave :("),
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +36,62 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="App">
+          <div className="container">
+            <div className="item">
+              <div className="overlay-effect"></div>
+              <Image
+                className="item-image"
+                src="https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+                alt="product"
+                width="100px"
+                height="100px"
+              />
+              <div className="item-details">
+                <p className="item-details__title">Coconut Oil</p>
+                <p className="item-details__amount">NGN{amount / 100}</p>
+              </div>
+            </div>
+            <div className="checkout">
+              <div className="checkout-form">
+                <div className="checkout-field">
+                  <label>Name</label>
+                  <input
+                    type="text"
+                    id="name"
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                  />
+                </div>
+                <div className="checkout-field">
+                  <label>Email</label>
+                  <input
+                    type="text"
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                  />
+                </div>
+                <div className="checkout-field">
+                  <label>Phone</label>
+                  <input
+                    type="text"
+                    id="phone"
+                    onChange={(e) => setPhone(e.target.value)}
+                    value={phone}
+                  />
+                </div>
+                <PaystackButton
+                  className="custom-checkout"
+                  {...componentProps}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
