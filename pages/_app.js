e87b592715e45be2804cfbd953b31c2eb6cb21c9
Seak Digital <nsineke@gmail.com>
2021-08-04 21:55:11 +0200

    feat: inti fullstory module

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Script from "next/script";
+
 import "../styles/globals.css";
 import "animate.css";
+
+import * as FullStory from "@fullstory/browser";
+
+FullStory.init({
+  orgId: process.env.NODE_ENV === "production" && process.env.FULLSTORY_ORG_ID,
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
