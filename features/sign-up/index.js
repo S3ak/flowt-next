50eba5611b8e6e7075dddb743f87bfacf51e5338
Seak Digital <nsineke@gmail.com>
2021-08-26 13:20:45 +0200

    ✨ feat(SignUp): Add fields

--- features/sign-up/index.js
+++ features/sign-up/index.js
@@ -1,64 +1,58 @@
 import { useFormik } from "formik";
+import { mutate } from "swr";
+
 import { FormField, Label, Input, Message, Form, Wrapper } from "./styled";
+import validationSchema, { signUpFormSchema } from "./validationSchema";
+import createAccount, { endPoint } from "./api";
 
-import validationSchema from "./validationSchema";
-
-const formSchema = [
-  {
-    id: "userName",
-    label: "User name",
-    type: "text",
-    placeholder: "You123",
-  },
-  {
-    id: "fullName",
-    label: "Full name",
-    type: "text",
-    placeholder: "Tsholo Mabaso",
-  },
-  {
-    id: "email",
-    label: "eMail",
-    type: "email",
-    placeholder: "you@mail.co.za",
-  },
-  {
-    id: "password",
-    label: "Password",
-    type: "password",
-    placeholder: "P@55W0rd1!",
-  },
-];
-
-export default function SingUp() {
+export default function SignUpForm() {
   const { handleSubmit, handleChange, values, errors, touched } = useFormik({
     initialValues: {
       fullName: "",
       userName: "",
       email: "",
       password: "",
+      mobileNumber: "",
     },
     validationSchema,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: ({ email, userName, fullName, mobileNumber }) => {
+      const nameArr = fullName.split(" ");
+      const firstName = nameArr[0];
+      const lastName = nameArr[nameArr.length - 1];
+
+      mutate(endPoint, {
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        handle: userName,
+        mobile_number: mobileNumber,
+      });
+
+      createAccount({
+        first_name: firstName,
+        last_name: lastName,
+        email,
+        handle: userName,
+        mobile_number: mobileNumber,
+      });
     },
   });
 
   return (
     <Wrapper>
       <Form onSubmit={handleSubmit}>
-        {formSchema.map(({ id, label, type, placeholder }) => {
+        {signUpFormSchema.map(({ id, label, type, placeholder }) => {
           return (
             <FormField key={id}>
               <Label htmlFor={id}>{label}</Label>
 
               <Input
-                type={type}
-                onChange={handleChange}
-                value={values[id]}
-                name={id}
                 id={id}
+                name={id}
+                type={type}
+                value={values[id]}
                 placeholder={placeholder}
+                onChange={handleChange}
               />
 
               {errors[id] && touched[id] && <Message>{errors[id]}</Message>}
