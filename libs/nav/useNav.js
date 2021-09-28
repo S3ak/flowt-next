4720e9cd7020a8723a7585045f652ee6eb2f024a
Seak Digital <nsineke@gmail.com>
2021-09-28 21:13:28 +0200

    🦄 refactor(useNav): relocate nav
    Nav list is failing on CI

--- libs/nav/useNav.js
+++ libs/nav/useNav.js
@@ -1,59 +1,6 @@
 import { useRouter } from "next/router";
 import { useState, useContext, createContext, useCallback } from "react";
 
-export const defaultNavItems = [
-  {
-    label: "Splash",
-    path: "/",
-    isVisible: false,
-  },
-  {
-    label: "Home",
-    path: "/home",
-    isVisible: true,
-  },
-  {
-    label: "Send Flowt",
-    path: "/send-flowt",
-    isVisible: true,
-  },
-  {
-    label: "Request Flowt",
-    path: "/request-flowt",
-    isVisible: true,
-  },
-  {
-    label: "Flowt Your Bowt",
-    path: "/flowt",
-    isVisible: true,
-  },
-  {
-    label: "Activity",
-    path: "/activity",
-    isVisible: true,
-  },
-  {
-    label: "Profile",
-    path: "/profile",
-    isVisible: true,
-  },
-  {
-    label: "Sign Up",
-    path: "/sign-up",
-    isVisible: false,
-  },
-  {
-    label: "Login",
-    path: "/sign-in",
-    isVisible: false,
-  },
-  {
-    label: "Forgot Password",
-    path: "/forgot-password",
-    isVisible: false,
-  },
-];
-
 const NavContext = createContext();
 
 export function findCurrentItemByPathName(list = [], pathName = "string") {
@@ -61,12 +8,62 @@ export function findCurrentItemByPathName(list = [], pathName = "string") {
 }
 
 export const useProvideNav = () => {
+  const items = [
+    {
+      label: "Splash",
+      path: "/",
+      isVisible: false,
+    },
+    {
+      label: "Home",
+      path: "/home",
+      isVisible: true,
+    },
+    {
+      label: "Send Flowt",
+      path: "/send-flowt",
+      isVisible: true,
+    },
+    {
+      label: "Request Flowt",
+      path: "/request-flowt",
+      isVisible: true,
+    },
+    {
+      label: "Flowt Your Bowt",
+      path: "/flowt",
+      isVisible: true,
+    },
+    {
+      label: "Activity",
+      path: "/activity",
+      isVisible: true,
+    },
+    {
+      label: "Profile",
+      path: "/profile",
+      isVisible: true,
+    },
+    {
+      label: "Sign Up",
+      path: "/sign-up",
+      isVisible: false,
+    },
+    {
+      label: "Login",
+      path: "/sign-in",
+      isVisible: false,
+    },
+    {
+      label: "Forgot Password",
+      path: "/forgot-password",
+      isVisible: false,
+    },
+  ];
+
   const { pathname } = useRouter();
   const [isOpen, setIsOpen] = useState(false);
-  const items = defaultNavItems;
-  const selectedItem = useCallback(findCurrentItemByPathName(items, pathname), [
-    pathname,
-  ]);
+  const selectedItem = findCurrentItemByPathName(items, pathname);
   const toggleNav = useCallback(() => {
     setIsOpen(!isOpen);
   }, [isOpen]);
