498c81a6e0ec3f5363cc1c65eae9ec65f610e01e
Seak Digital <nsineke@gmail.com>
2021-09-29 08:51:59 +0200

    🔧 build(jest.config): Ignore cypress

--- jest.config.js
+++ jest.config.js
@@ -173,7 +173,11 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.next/",
+    "<rootDir>/cypress/",
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
