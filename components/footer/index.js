0bc343abb18803de4965608b9674a2327e5b5c87
Monde Sineke <nsineke@gmail.com>
2022-05-12 15:11:36 +0200

    feat: unknown changes

--- components/footer/index.js
+++ components/footer/index.js
@@ -8,3 +8,40 @@ const Footer = styled.div`
 `;
 
 export default Footer;
+
+const DEFAULT_PAGES = [{
+  title: 'help',
+  url: '/help',
+  altText: 'Help',
+  pages: [{
+    title: "Contact Us",
+    url: "/contact-us",
+    altText: "Contact us"
+  }],
+  title: 'Home',
+  url: '/home',
+  altText: 'home',
+  pages: []
+}];
+
+const StyledFooterLink = (page) => {
+  return (
+    <div>
+      {page}
+    </div>
+  )
+}
+
+const GlobalFooter = (props) => {
+  const pages = props.pages ?? DEFAULT_PAGES
+
+  return (
+    <section className="c-global-footer">
+      <article>
+        {pages.map(page => {
+          <div key={page.url}><StyledFooterLink page={page} /></div>
+        })}       
+      </article>
+    </section>
+  )
+}
